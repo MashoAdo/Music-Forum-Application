78ac55c511fc7f5c2a92da012ee71afbaa7cccd8
Masho Ado <mashoado@gmail.com>
2021-12-18 03:41:58 +0300

    add and remove post modal functionality

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 const menu = document.getElementById("menu");
 const menuBtn = document.getElementById("menu-btn");
+const createPostButton = document.getElementById("create-post");
+const postModal = document.getElementById("post-modal");
+const overlay = document.getElementById("overlay");
+const postModalButton = document.getElementById("post-modal-button");
 
 // show/remove menu on click
 menuBtn.addEventListener("click", () => {
 	menu.classList.toggle("show");
+});
+
+// add create post modal and add overlay
+createPostButton.addEventListener("click", () => {
+	postModal.classList.toggle("active");
+	overlay.classList.toggle("active");
+	createPostButton.classList.toggle("active");
+});
+
+// remove create post modal and add overlay
+postModalButton.addEventListener("click", () => {
+	postModal.classList.remove("active");
+	overlay.classList.remove("active");
 });
